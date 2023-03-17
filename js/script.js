d81b1f30a107ee152a45d37f9426d24750e175b6
TynkersPG <127526854+Tynkers@users.noreply.github.com>
2023-03-17 16:18:44 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+
+
+
+function Mudarestado(el) {
+    
+   
+   
+    var jogo2 = document.getElementById("jogo2").style.display='block';
+    var jogo1 = document.getElementById("jogo1").style.display='none';
+    var btn2 = document.getElementById("btn2").style.display='block'
+   
+
+   
+      
+        
+   
+}
+
+
+function Mudarestado1(el) {
+    
+   
+   
+    var jogo2 = document.getElementById("jogo2").style.display='none';
+    var jogo3 = document.getElementById("jogo3").style.display='block';
+    
+   
+
+   
+      
+        
+   
+}
+
